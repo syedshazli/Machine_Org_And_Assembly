6575fe66d2b12c9347593e5fa16f3f2b28d2a156
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2025-03-23 23:31:10 -0400

    Update bits.c

--- Datalab/bits.c
+++ Datalab/bits.c
@@ -2,3 +2,17 @@ int bitXOR(int x, int y){
 
   return (~ (~(x&~y) & ~(~x & y )));
 }
+
+int tmin(void){
+  int x = 1;
+  return x <<31;
+}
+
+int isTmax(int x){
+  int compare = 1;
+  compare = compare << 31;
+  compare = ~compare;
+
+  int xor = compare ^ x;
+  return !(xor);
+}
